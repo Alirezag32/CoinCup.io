068e266518bb44d7bad4b4505cdaaf69d81a5b2d
Alirezag <grawndlyrda3@gmail.com>
2024-09-23 17:02:06 +0330

    Upload two

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,17 +4,26 @@ import "@fortawesome/fontawesome-svg-core/styles.css";
 config.autoAddCss = false;
 import { MyProvider } from "@/providers/maiContext";
 import { Inter } from "next/font/google";
-
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"], weight: ["400", "700"] });
 
 
 export default function App({ Component, pageProps }) {
    const getLayout = Component.getLayout ?? ((page) => page);
-   return (
-     <div className={inter.className}>
-       <MyProvider>{getLayout(<Component {...pageProps} />)}</MyProvider>
-       {/* <ToastContainer /> */}
-     </div>
-   );
+  return (
+    <>
+      <Head>
+        <title>coinCup</title>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover"
+        />
+      </Head>
+      <div className={inter.className}>
+        <MyProvider>{getLayout(<Component {...pageProps} />)}</MyProvider>
+        {/* <ToastContainer /> */}
+      </div>
+    </>
+  );
 }
