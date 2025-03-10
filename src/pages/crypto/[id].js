5ef8a486e414b4dc2a1d9f23576b06704bf60fb3
Alirezag <grawndlyrda3@gmail.com>
2025-03-10 09:12:02 +0330

    upload 24

--- src/pages/crypto/[id].js
+++ src/pages/crypto/[id].js
@@ -52,7 +52,7 @@ export default function CryptoDetail() {
           ),
           datasets: [
             {
-              label: 'price in this day (dollar)',
+              label: 'price in 7 days (dollar)',
               data: chartData.prices.map((p) => p[1]),
               borderColor: "#1DB954",
               backgroundColor: "rgba(29, 185, 84, 0.2)",
